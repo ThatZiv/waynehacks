5850a369e0cc8ead047c155182c275ab02429d36
ThatZiv <thatziv@gmail.com>
2023-09-08 14:42:42 -0400

    added forgot password functionality

---
+++ actions/forgetPassword.ts
@@ -0,0 +1,20 @@
+"use server";
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export const resetPassword = async (e: FormData) => {
+  const email = String(e.get("email"));
+  const captcha = String(e.get("captcha"));
+
+  const supabase = createServerActionClient({ cookies });
+
+  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo:
+      (process.env.NEXT_PUBLIC_BASE_URL || "https://waynehacks.com") +
+      "/auth/callback?next=/login/forgot",
+    captchaToken: captcha,
+  });
+  if (error) redirect("/login?error=" + error.message);
+  redirect("/?message=Please check your email for resetting your password");
+};
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,8 +4,14 @@ import Messages from "../../components/messages";
 import Back from "@/components/Back";
 import useCaptcha from "@/components/useCaptcha";
 import WayneHacksLogo from "@/components/WayneHacksLogo";
+import React from "react";
+import { resetPassword } from "@/actions/forgetPassword";
+
 export default function Login() {
+  const [showForgetPassword, setForgetPassword] =
+    React.useState<boolean>(false);
   const { HCaptcha, isLoading, token, setToken } = useCaptcha();
+
   return (
     <div className="flex-1 animate-in flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Back />
@@ -28,42 +34,64 @@ export default function Login() {
           placeholder="you@example.com"
           required
         />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
+        {!showForgetPassword && (
+          <>
+            <label className="text-md" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="rounded-md px-4 py-2 bg-inherit border mb-6"
+              type="password"
+              name="password"
+              placeholder="••••••••"
+              required
+            />
+          </>
+        )}
+
         <div className="flex flex-col w-full items-center">
           {!token && <HCaptcha />}
         </div>
-        {token && (
-          <>
-            <button
-              className="bg-green-900 rounded px-4 py-2 text-white mb-2"
-              disabled={isLoading || !token}
-            >
-              Sign In
-            </button>
-            <input type="hidden" name="captcha" value={token ?? ""} />
-            <button
-              formAction="/auth/sign-up"
-              className="border border-gray-700 rounded px-4 py-2 mb-2"
-              onSubmit={(e) => {
-                e.preventDefault();
-                setToken(null);
-              }}
-              disabled={isLoading || !token}
-            >
-              Sign Up
-            </button>
-          </>
-        )}
+        {token &&
+          (showForgetPassword ? (
+            <>
+              <button
+                className="bg-green-900 rounded px-4 py-2 text-white mb-2"
+                formAction={resetPassword}
+              >
+                Send recovery to email
+              </button>
+              <input type="hidden" name="captcha" value={token ?? ""} />
+            </>
+          ) : (
+            <>
+              <button
+                className="bg-green-900 rounded px-4 py-2 text-white mb-2"
+                disabled={isLoading || !token}
+              >
+                Sign In
+              </button>
+              <input type="hidden" name="captcha" value={token ?? ""} />
+              <button
+                formAction="/auth/sign-up"
+                className="border border-gray-700 rounded px-4 py-2 mb-2"
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  setToken(null);
+                }}
+                disabled={isLoading || !token}
+              >
+                Sign Up
+              </button>
+            </>
+          ))}
       </form>
+      <p
+        onClick={() => setForgetPassword((s) => !s)}
+        className="text-center text-green-800 hover:underline cursor-pointer"
+      >
+        Forgot password?
+      </p>
     </div>
   );
 }
