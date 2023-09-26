9bb6eac582b47e1d670e39368cfd2d7c67e085b9
Zavaar Shah <thatziv@gmail.com>
2023-09-26 17:09:59 -0400

    sign in redir and form statuses

--- app/auth/sign-in/route.ts
+++ app/auth/sign-in/route.ts
@@ -10,6 +10,7 @@ export async function POST(request: Request) {
   const email = String(formData.get("email"));
   const password = String(formData.get("password"));
   const captcha = String(formData.get("captcha"));
+  const next = requestUrl.searchParams.get("next");
   const supabase = createRouteHandlerClient({ cookies });
 
   const { error } = await supabase.auth.signInWithPassword({
@@ -31,7 +32,7 @@ export async function POST(request: Request) {
   }
 
   return NextResponse.redirect(
-    requestUrl.origin,
+    requestUrl.origin + (next || ""),
     {
       // a 301 status is required to redirect from a POST to a GET route
       status: 301,
--- components/Registered.tsx
+++ components/Registered.tsx
@@ -8,57 +8,61 @@ export const revalidate = 60 * 30; // cache expires every 30 mins
 
 export default async function Registered() {
   "use server";
-  const supabase = createServerComponentClient({ cookies });
-  const whacks = new SupabaseFunctions(supabase);
-  const canRegister = await whacks.getConfigValue("canRegister");
-  console.log(canRegister);
-  if (!canRegister)
-    return (
-      <Link
-        href="/login?signup=true"
-        className="bg-foreground py-3 px-6 rounded-lg font-mono text-xlg font-bold text-background transition-all hover:bg-[#fc0] hover:px-12"
-      >
-        Sign Up
-      </Link>
-    );
-  const applicants = await whacks.getApplicants();
-  return (
-    <div>
-      <div className="flex items-center text-4xl mb-6">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-14 h-17"
+  try {
+    const supabase = createServerComponentClient({ cookies });
+    const whacks = new SupabaseFunctions(supabase);
+    const canRegister = await whacks.getConfigValue("canRegister");
+    if (!canRegister)
+      return (
+        <Link
+          href="/login?signup=true"
+          className="bg-foreground py-3 px-6 rounded-lg font-mono text-xlg font-bold text-background transition-all hover:bg-[#fc0] hover:px-12"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-          />
-        </svg>
-        <div className="">
-          {applicants ? (
-            <>
-              <span className="animate-pulse">
-                <strong>{applicants}</strong>
-              </span>
-              <br />
-            </>
-          ) : (
-            <Spinner />
-          )}
-          <p className="text-sm mb-1">registered</p>
+          Sign Up
+        </Link>
+      );
+    const applicants = await whacks.getApplicants();
+    return (
+      <div>
+        <div className="flex items-center text-4xl mb-6">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-14 h-17"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+            />
+          </svg>
+          <div className="">
+            {applicants ? (
+              <>
+                <span className="animate-pulse">
+                  <strong>{applicants}</strong>
+                </span>
+                <br />
+              </>
+            ) : (
+              <Spinner />
+            )}
+            <p className="text-sm mb-1">registered</p>
+          </div>
         </div>
+        <Link
+          href="/application"
+          className="bg-foreground py-3 px-11 rounded-lg font-mono text-xlg font-bold text-background transition-all hover:bg-[#fc0] hover:px-16"
+        >
+          Apply
+        </Link>
       </div>
-      <Link
-        href="/application"
-        className="bg-foreground py-3 px-11 rounded-lg font-mono text-xlg font-bold text-background transition-all hover:bg-[#fc0] hover:px-16"
-      >
-        Apply
-      </Link>
-    </div>
-  );
+    );
+  } catch (error: any) {
+    console.error(error.message);
+    return <div>{error.message}</div>;
+  }
 }
