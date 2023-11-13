840c457c6ad87c1498c1398c26c0b2d67890c098
Zavaar Shah <thatziv@gmail.com>
2023-11-13 01:31:12 -0500

    clean

--- app/auth/sign-in/route.ts
+++ app/auth/sign-in/route.ts
@@ -11,7 +11,6 @@ export async function POST(request: Request) {
   const password = String(formData.get("password"));
   const captcha = String(formData.get("captcha"));
   const next = requestUrl.searchParams.get("next");
-  console.log(next);
   const supabase = createRouteHandlerClient({ cookies });
 
   if (!email || !password || !captcha) {
