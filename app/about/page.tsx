c813c8f845b82708997d564e8899d56e6c4d1399
Cadek2002 <77899221+Cadek2002@users.noreply.github.com>
2023-11-19 12:52:48 -0500

    Centered Organizers Section Added About Us blip

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,10 +7,10 @@ import Announcement from "@/components/Announcement";
 import constants from "@/misc/constants";
 
 export const revalidate = 15 * 60; // revalidate every 15 min
-export const dynamic = "force-static"; // force static (this wont work)
+export const dynamic = "force-static"; // force static (this won't work)
 
 const colClass = "w-48 h-72"
-const imageContainerClass = "h-64 w-48 border-8 p-2 ml-20 mr-20"
+const imageContainerClass = "h-64 w-48 border-8 p-2 mr-20"
 const imageClass = "h-full w-full object-cover"
 
 export default async function Index() {
@@ -21,10 +21,18 @@ export default async function Index() {
   // } = await supabase.auth.getUser();
   return (
     <div className="w-full flex flex-col items-center">
-      <div className="animate-in gap-14 opacity-0 max-w-4xl px-3 py-16 lg:py-24 text-white items-center">
-        <h1 className="text-center">About Us</h1>
+      <div className="animate-in gap-14 opacity-0 max-w-4xl px-3 py-16 lg:py-24 text-white">
+        <h1 className="text-center text-3xl pb-5">About Us</h1>
+        <p className="text-center  text-2px">
+          Established in Fall 2021 as Wayne State's only Computer Science organization, SCD is focused on creating opportunities
+          for students interested in coding and development. We aim to provide a community of peers that support one another in
+          coding, resume building, networking, and developing new skills.<br/>
+          WayneHacks is one of those efforts, while past events have been hosted online, this is our first step in transitioning
+          to an in-person event.
+        </p>
+        <h2 className="text-center text-2xl pb-5 pt-2">Organizers</h2>
         <div className="w-auto"/>
-        <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-5 justify-self-center">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-5">
           <div className={colClass}><div className={imageContainerClass}><img src="/res/Zavaar_Shah.jpg" alt="Headshot of Zavaar Shah" className={imageClass}/><span className="items-center w-48 ">Zavaar Shah</span></div></div>
           <div className={colClass}><div className={imageContainerClass}><img src="/res/Cayden_Koweck.jpg" alt="Headshot of Cayden Koweck" className={imageClass}/><span className="items-center w-48 ">Cayden Koweck</span></div></div>
           <div className={colClass}><div className={imageContainerClass}><img src="/res/Madeline_Bartley.jpg" alt="Headshot of Madeline Bartley" className={imageClass}/><span className="items-center w-48 ">Madeline Bartley</span></div></div>
