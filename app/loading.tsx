8cea419b5c17975705b86f9df02e4c04075673bd
Zavaar Shah <thatziv@gmail.com>
2023-11-20 16:30:35 -0500

    loader restyling

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 function loading() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900" />
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-600" />
     </div>
   );
 }
