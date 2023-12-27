e09554adc11ed1a94770aaca53df65a46ecd533e
ThatZiv <thatziv@gmail.com>
2023-12-27 00:10:12 -0500

    admin panel realtime updates

--- misc/application.ts
+++ misc/application.ts
@@ -1,35 +1,44 @@
 type Modify<T, R> = Omit<T, keyof R> & R;
 
+/**
+ * 'applications' table schema
+ */
 export interface Application {
-    applicant_id: string;
-    created_at: string | Date;
-    full_name: string;
-    graduation_year: string;
-    major: string;
-    university: string;
-    phone_number?: string;
-    diet: string;
-    student_id: string;
-    email: string;
-    status?: status // this is an relation expansion
-}
-export interface status {
-    applicant_id: string;
-    status: statusEnum;
-    note: string
-    created_at: string | Date;
-    modified_at: string | Date;
+  applicant_id: string;
+  created_at: string | Date;
+  full_name: string;
+  graduation_year: string;
+  major: string;
+  university: string;
+  phone_number?: string;
+  diet: string;
+  student_id: string;
+  email: string | "none";
+  status?: Status; // this is an relation expansion
 }
 
-export type ApplicationWithStatus = Modify<Application, {
-    status: statusEnum
-    applications: Application
-}>
+/**
+ * 'status' table schema
+ */
+export interface Status {
+  applicant_id: string;
+  status: statusEnum;
+  note: string;
+  checked_in: boolean;
+  created_at: string | Date;
+  modified_at: string | Date;
+}
 
+/**
+ * Application w/ status used in admin panel
+ */
+export interface StatusApplication extends Status {
+  applications: Application; // relation expansion
+}
 export enum statusEnum {
-    APPLIED = "applied",
-    ACCEPTED = "accepted",
-    REJECTED = "rejected",
-    WAITLISTED = "waitlisted",
-    CANCELLED = "cancelled"
+  APPLIED = "applied",
+  ACCEPTED = "accepted",
+  REJECTED = "rejected",
+  WAITLISTED = "waitlisted",
+  CANCELLED = "cancelled",
 }
