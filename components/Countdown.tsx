67732f912549387b6eeb4729d414386a5da1a8cb
ThatZiv <thatziv@gmail.com>
2024-01-06 14:55:54 -0500

    made schedule preloader

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -33,7 +33,7 @@ export const Countdown = () => {
   });
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    const tick = () => {
       const now = new Date().getTime();
       const upcomingEvents = eventData.filter((event) => event.date > now);
 
@@ -49,61 +49,74 @@ export const Countdown = () => {
         setTime({ days, hours, minutes, seconds });
         setCurrentEvent(nextEvent.name);
       } else {
-        // All events have passed
+        // if we are in the interval clear it
         clearInterval(interval);
-        setCurrentEvent("All events passed");
+        setCurrentEvent("This event has ended.");
       }
-    }, 1000);
+    };
+    const interval = setInterval(tick, 1000);
+    tick();
 
     return () => clearInterval(interval);
   }, [eventData]);
 
-  if (
+  const isComplete =
     time.days === 0 &&
     time.hours === 0 &&
     time.minutes === 0 &&
-    time.seconds === 0
-  )
-    return null;
+    time.seconds === 0;
   if (!eventData) return null;
+  if (currentEvent === "Loading...") return null;
   return (
     <Link href="events.ics">
       <div className="flex transition-all animate-in flex-col countdown">
-        <div className="flex flex-col justify-center gap-2">
-          <div className="flex flex-row gap-12 justify-between items-center md:justify-center ">
-            <div className="flex flex-col items-center">
-              <p className="text-4xl">{time.days}</p>
-              <p className="text-md">Days</p>
+        {!isComplete ? (
+          <>
+            <div className="flex flex-col justify-center gap-2">
+              <div className="flex flex-row gap-12 justify-between items-center md:justify-center ">
+                <div className="flex flex-col items-center">
+                  <p className="text-4xl">{time.days}</p>
+                  <p className="text-md">Days</p>
+                </div>
+                <div className="flex flex-col items-center">
+                  <p className="text-4xl">{time.hours}</p>
+                  <p className="text-md">Hours</p>
+                </div>
+                <div className="flex flex-col items-center">
+                  <p className="text-4xl">{time.minutes}</p>
+                  <p className="text-md">Minutes</p>
+                </div>
+                <div className="flex flex-col items-center">
+                  <p className="text-4xl">{time.seconds}</p>
+                  <p className="text-md">Seconds</p>
+                </div>
+              </div>
             </div>
             <div className="flex flex-col items-center">
-              <p className="text-4xl">{time.hours}</p>
-              <p className="text-md">Hours</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <p className="text-4xl">{time.minutes}</p>
-              <p className="text-md">Minutes</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <p className="text-4xl">{time.seconds}</p>
-              <p className="text-md">Seconds</p>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col items-center">
-          {/* add a line splitter */}
-          <hr className="w-full border-gray-500 border-1 my-2" />
+              {/* add a line splitter */}
+              <hr className="w-full border-gray-500 border-1 my-2" />
 
-          <div className="text-xs text-gray-500 ">
-            <span className="font-extrabold">{currentEvent}</span>
-            {` • ${getHumanReadableDate(
-              (
-                eventData.find((event) => event.name === currentEvent) || {
-                  date: new Date().getTime(),
-                }
-              ).date || ""
-            )}`}
-          </div>
-        </div>
+              <div className="text-xs text-gray-500 ">
+                <span className="font-extrabold">{currentEvent}</span>
+                {` • ${getHumanReadableDate(
+                  (
+                    eventData.find((event) => event.name === currentEvent) || {
+                      date: new Date().getTime(),
+                    }
+                  ).date || ""
+                )}`}
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="flex flex-col justify-center gap-2">
+              <div className="flex flex-row gap-12 justify-between items-center md:justify-center ">
+                <h2>This event has ended.</h2>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </Link>
   );
