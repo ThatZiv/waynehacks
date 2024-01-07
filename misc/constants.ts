bd114036f94c39c85523497b327d9a3442acf2e1
ThatZiv <thatziv@gmail.com>
2024-01-06 21:46:14 -0500

    dates are initialized AS est date

--- misc/constants.ts
+++ misc/constants.ts
@@ -1,58 +1,65 @@
 import { Event } from "./events";
 
+// will convert date string into an EST date timestamp
+export const _ = (date: string) => {
+  let d = new Date(date);
+  d.setMinutes(d.getMinutes() + d.getTimezoneOffset() - 300);
+  return d.getTime();
+};
+
 /**
  * Order matters
  */
 export const events: Event[] = [
   {
-    date: new Date("January 13, 2024 9:00:00").getTime(),
-    end: new Date("January 13, 2024 11:00:00").getTime(),
+    date: _("January 13, 2024 9:00:00"),
+    end: _("January 13, 2024 11:00:00"),
     name: "Check-in",
   },
   {
-    date: new Date("January 13, 2024 11:00:00").getTime(),
-    end: new Date("January 13, 2024 12:00:00").getTime(),
+    date: _("January 13, 2024 11:00:00"),
+    end: _("January 13, 2024 12:00:00"),
     name: "Opening Ceremony",
   },
   {
-    date: new Date("January 13, 2024 12:00:00").getTime(),
+    date: _("January 13, 2024 12:00:00"),
     name: "Hacking Begins",
   },
   {
-    date: new Date("January 13, 2024 12:10:00").getTime(),
-    end: new Date("January 13, 2024 13:00:00").getTime(),
+    date: _("January 13, 2024 12:10:00"),
+    end: _("January 13, 2024 13:00:00"),
     name: "Team Matchmaking",
   },
   {
-    date: new Date("January 13, 2024 17:00:00").getTime(),
-    end: new Date("January 13, 2024 19:00:00").getTime(),
+    date: _("January 13, 2024 17:00:00"),
+    end: _("January 13, 2024 19:00:00"),
     name: "Workshop Event",
   },
   {
-    date: new Date("January 13, 2024 23:00:00").getTime(),
+    date: _("January 13, 2024 23:00:00"),
     name: "Building Closes",
   },
   {
-    date: new Date("January 14, 2024 8:00:00").getTime(),
+    date: _("January 14, 2024 8:00:00"),
     name: "Building Opens",
   },
   {
-    date: new Date("January 14, 2024 11:00:00").getTime(),
-    end: new Date("January 14, 2024 12:00:00").getTime(),
+    date: _("January 14, 2024 11:00:00"),
+    end: _("January 14, 2024 12:00:00"),
     name: "Last Minute Help for Submissions",
   },
   {
-    date: new Date("January 14, 2024 12:00:00").getTime(),
+    date: _("January 14, 2024 12:00:00"),
     name: "Hacking Ends",
   },
   {
-    date: new Date("January 14, 2024 12:30:00").getTime(),
-    end: new Date("January 14, 2024 14:30:00").getTime(),
+    date: _("January 14, 2024 12:30:00"),
+    end: _("January 14, 2024 14:30:00"),
     name: "Presentations/Judging",
   },
   {
-    date: new Date("January 14, 2024 14:30:00").getTime(),
-    end: new Date("January 14, 2024 15:00:00").getTime(),
+    date: _("January 14, 2024 14:30:00"),
+    end: _("January 14, 2024 15:00:00"),
     name: "Closing Ceremony",
   },
 ];
