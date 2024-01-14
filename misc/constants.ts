a53dda8ef5c3062c84b1322bda6bdb35e52ebaf3
ThatZiv <thatziv@gmail.com>
2024-01-14 00:12:04 -0500

    change building open time

--- misc/constants.ts
+++ misc/constants.ts
@@ -48,7 +48,7 @@ export const events: Event[] = [
     name: "Building Closes",
   },
   {
-    date: _("January 14, 2024 8:00:00"),
+    date: _("January 14, 2024 10:00:00"),
     name: "Building Opens",
   },
   {
