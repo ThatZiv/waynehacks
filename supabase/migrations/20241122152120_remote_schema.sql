14e2c2e5a264f04fa79b2651dd6c518aa5b952e5
ThatZiv <thatziv@gmail.com>
2024-11-22 10:25:29 -0500

    shirt size to application schema + compliant psql function changes

---
+++ supabase/migrations/20241122152120_remote_schema.sql
@@ -0,0 +1,30 @@
+alter table "public"."applications" add column "shirt_size" character varying;
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_value(key_id text)
+ RETURNS jsonb
+ LANGUAGE plpgsql
+AS $function$DECLARE
+  result TEXT;
+BEGIN
+  EXECUTE 'select value from kv where key = $1' INTO result USING key_id;
+  RETURN result;
+END;$function$
+;
+
+CREATE OR REPLACE FUNCTION public.is_admin(uid uuid)
+ RETURNS boolean
+ LANGUAGE plpgsql
+ SET search_path TO ''
+AS $function$
+declare
+  result BOOLEAN;
+BEGIN
+  EXECUTE 'SELECT EXISTS (SELECT 1 FROM public.admins WHERE id = $1 AND is_admin = true)' INTO result USING uid;
+  RETURN result;
+END;
+$function$
+;
+
+
