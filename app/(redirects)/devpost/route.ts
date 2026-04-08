048d5653a64e03ac82e66fb9db323f3cd80fd4ba
Zavaar Shah <thatziv@gmail.com>
2026-04-08 00:45:06 -0400

    permenent redirects

--- app/(redirects)/devpost/route.ts
+++ app/(redirects)/devpost/route.ts
@@ -2,5 +2,5 @@ import constants from "@/misc/constants";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  return NextResponse.redirect(constants.devpost, { status: 302 });
+  return NextResponse.redirect(constants.devpost, { status: 301 });
 }
